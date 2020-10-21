29481a6501b5e37f026978fee10c8d2c0a1df43c
Flávio José Formis <flavio.formis@pca.com.br>
2020-10-21 14:34:20 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Deconstructors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var p = new Pessoa{
+                Nome = "Flávio",
+                SobreNome = "Formis"
+            };
+
+            (string x, string y) = p;
+
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+        }
+    }
+
+    public class Pessoa{
+        public string Nome {get;set;}
+        public string SobreNome {get;set;}
+
+        public void Deconstruct(out string nome, out string sobrenome) => (nome, sobrenome) = (Nome, SobreNome);
+    }
+}
